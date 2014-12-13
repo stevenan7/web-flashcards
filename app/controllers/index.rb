48469921f083670d6be98f869e2ccbc6cda8097f
Steven An <stevenan7@gmail.com>
2014-12-13 14:32:04 -0600

    added login functionality

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,3 +4,19 @@ end
 
 get '/login' do
   erb :'/login'
+end
+
+post '/login' do
+	@user = User.find_by(name: params[:name])
+	if @user && @user.password == params[:password]
+		session[:user_id] = @user.id
+		redirect "users/#{@user.id}"
+	else
+		redirect '/login'
+	end
+end
+
+get '/users/:user_id' do
+
+	erb :user 
+end
